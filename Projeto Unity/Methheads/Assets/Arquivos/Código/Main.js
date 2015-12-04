a9a3881705d5331338cdbd8aae944c064c840364
Gabriel Cintra <gabriel.cintra@ccc.ufcg.edu.br>
2015-12-03 22:57:36 -0300

    Revert "DEAD COMMIT"
    This reverts commit 1a96ca29251578ec806a32d3e0a6a680e925508a.

---
+++ Projeto Unity/Methheads/Assets/Arquivos/Código/Main.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+class Main extends MonoBehaviour {
+	
+	private var methProduzida : float;
+	
+	function Start() {
+	}
+	
+	function Update(){
+	}
+	
+	function produzir() {
+		methProduzida++;
+		print (methProduzida);
+	}
+	
+	function produzirAutomaticamente() {
+	}
+	
+	function setMethProduzida(methProduzida : float) {
+		this.methProduzida = methProduzida;
+	}
+	
+}
