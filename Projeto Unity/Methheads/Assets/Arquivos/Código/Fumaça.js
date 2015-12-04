6829ccbedc1a709f7016370a880082dcc10b0c10
iluketz <jornalformice@hotmail.com>
2015-12-03 22:49:58 -0300

    MenuPrincipal.unity
    Maioria da interface pronta, falta apenas os códigos;
    Smoke interativo;

---
+++ Projeto Unity/Methheads/Assets/Arquivos/Código/Fumaça.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var smoke : GameObject;
+
+function Start () {
+smoke = GameObject.Find("WhiteSmoke");
+smoke.SetActive(false);
+}
+
+function Update () {
+
+}
+
+function OnMouseEnter(){
+smoke.SetActive(true); 
+}
+
+function OnMouseExit(){
+smoke.SetActive(false);
+}
