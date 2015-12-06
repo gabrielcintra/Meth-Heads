afec26266588f18f38c78a94a84b5cac2b41bc22
iluketz <jornalformice@hotmail.com>
2015-12-06 19:28:13 -0300

    Ambiente Quarto
    -Sistema de rádio;
    -Quarto ajustado;

---
+++ Projeto Unity/Methheads/Assets/Arquivos/codigo/radio.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var menuRadio : GameObject;
+var audioManager : AudioSource;
+var sons : AudioClip[];
+var menuAberto : boolean;
+
+
+///todas as declarações foram feitas no inspector
+
+function Start () {
+    menuRadio.SetActive(false);
+    menuAberto = false;
+}
+
+function iniciar(){
+    audioManager.UnPause();
+}
+
+function pausar(){
+    audioManager.Pause();
+}
+
+function passar(){
+    var indexMusicaAtual = sons.IndexOf(sons,audioManager.clip);
+    
+    if (indexMusicaAtual + 1 < sons.length){
+        audioManager.clip = sons[indexMusicaAtual+1];
+        audioManager.Play();
+    }
+    else{
+        audioManager.clip = sons[0];
+        audioManager.Play();
+    }
+}
+
+function abrirFecharMenu(){
+    if (!menuAberto){
+        menuRadio.SetActive(true);
+        menuAberto = true;
+    }
+    else{
+        menuRadio.SetActive(false);
+        menuAberto = false;
+    }
+}
