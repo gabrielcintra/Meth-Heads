f1d81397d0af042e74df1fe88dfb380999136b74
iluketz <jornalformice@hotmail.com>
2015-12-25 05:27:40 -0300

    mudanças na interface

---
+++ Projeto Unity/Methheads/Assets/Arquivos/codigo/funcoes/autoDesativar.js
@@ -0,0 +1,11 @@
+﻿var object : GameObject;
+function ativar(objeto : GameObject){
+    object = objeto;
+    objeto.SetActive(true);
+    Invoke("desativar",3);
+}
+
+function desativar(){
+    object.SetActive(false);
+    CancelInvoke("desativar");
+}
